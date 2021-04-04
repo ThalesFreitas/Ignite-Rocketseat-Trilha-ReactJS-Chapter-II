b421abce72eabb6ba9e85841b09d3dc90cddfaeb
Thales Freitas <thalesfreitas10@gmail.com>
2021-04-04 16:16:45 -0300

    Cores dos botões

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {darken} from 'polished';
+import {darken, transparentize} from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -63,7 +63,13 @@ gap: 0.5rem;
 
 interface RadioBoxProps {
     isActive: boolean;
+    activeColor: 'green' | 'red';
 }
+
+const colors = {
+    green: '#33CC95',
+    red: '#E52E4D'
+};
 
 export const RadioBox = styled.button<RadioBoxProps>`
   height: 4rem;
@@ -71,7 +77,9 @@ export const RadioBox = styled.button<RadioBoxProps>`
     border-radius: 0.25rem;
 
     //Se minha isActive for true, então muda a cor do botão se não deixa transparente
-    background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+    background: ${(props) => props.isActive 
+    ? transparentize(0.9, colors[props.activeColor]) 
+    : 'transparent'};
 
     display: flex;
     align-items: center;
